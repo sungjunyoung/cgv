07f97e23005840ec06f4ccd186e2a4857d69ef7d
Junyoung, Sung <wnsdud1861@gmail.com>
2023-04-16 18:07:43 +0900

    Initial commit

---
+++ src/main/kotlin/dev/sungjunyoung/cgv/CgvApplication.kt
@@ -0,0 +1,11 @@
+package dev.sungjunyoung.cgv
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CgvApplication
+
+fun main(args: Array<String>) {
+	runApplication<CgvApplication>(*args)
+}
